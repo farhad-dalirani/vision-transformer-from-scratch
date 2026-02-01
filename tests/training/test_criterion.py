8d24fda96121bfe154f7f1a0f53b9ffe3e417179
Farhad <dalirani.1373@gmail.com>
2026-01-31 23:17:06 -0500

    test(training): added tests for loss functions.

---
+++ tests/training/test_criterion.py
@@ -0,0 +1,46 @@
+import pytest
+import torch
+import torch.nn as nn
+from torch.nn import CrossEntropyLoss
+
+from vision_transformer.training.losses import (
+    calculate_loss,
+    criterion,
+    cross_entropy_loss,
+)
+
+
+def test_wrong_criterion_name():
+
+    with pytest.raises(ValueError):
+        criterion(type="wrong-criterion-name")
+
+
+def test_get_cross_entropy_criterion():
+
+    crt = criterion(type="cross-entropy")
+    assert isinstance(crt, CrossEntropyLoss)
+
+
+def test_cross_entropy_set_hyperparameters():
+
+    crt = criterion(type="cross-entropy", label_smoothing=0.2, reduction="sum")
+    assert crt.label_smoothing == 0.2
+    assert crt.reduction == "sum"
+
+
+def test_calculate_loss():
+    crt = criterion(type="cross-entropy")
+
+    logit_pred = torch.rand(size=(4, 10))
+    y_gt = torch.randint(low=0, high=10, size=(4,))
+
+    loss = calculate_loss(
+        criterion=crt,
+        logit_pred=logit_pred,
+        y_gt=y_gt,
+    )
+
+    assert isinstance(loss, torch.Tensor)
+    assert loss.dim() == 0
+    assert torch.isfinite(loss)
