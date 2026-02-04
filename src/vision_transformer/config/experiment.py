d5a4a455aaa0d3d0bdb40469a2b664b43066c401
Farhad <dalirani.1373@gmail.com>
2026-02-03 23:47:44 -0500

    chore: updated confgis.

--- src/vision_transformer/config/experiment.py
+++ src/vision_transformer/config/experiment.py
@@ -3,10 +3,10 @@ from dataclasses import dataclass
 from vision_transformer.config.dataset import DatasetConfig
 from vision_transformer.config.loss import LossConfig
 from vision_transformer.config.lr_scheduler import LRSchedulerConfig
+from vision_transformer.config.model import ViTConfig
 from vision_transformer.config.optimizer import OptimizerConfig
 from vision_transformer.config.training import TrainingConfig
 from vision_transformer.config.transform import TransformConfig
-from vision_transformer.config.model import ViTConfig
 
 
 @dataclass(frozen=True)
